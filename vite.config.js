0ac2c3044dbccdbde5d3446ef52f4acf7d243428
jayesh-jain-11 <jayeshrlhj.r@gmail.com>
2025-12-27 09:12:12 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      // Tell Vite where to find the browser version of monaco-languageclient
+      "monaco-languageclient/lib/browser":
+        "monaco-languageclient/es/browser/index.js",
+    },
+  },
+});
